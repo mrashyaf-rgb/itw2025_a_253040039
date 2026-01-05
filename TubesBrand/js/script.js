958db8b191759d7b47bc5ede8bee9acfb58f9d84
mrashyaf-rgb <muhammadrasyafirmansyah46@gmail.com>
2026-01-05 22:38:51 +0700

    Finish Tubes2

---
+++ TubesBrand/js/script.js
@@ -0,0 +1,20 @@
+// Toggle Navbar Mobile
+const menuBtn = document.getElementById("menuBtn");
+const mobileMenu = document.getElementById("mobileMenu");
+
+menuBtn.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
+
+// Scroll Animation
+const faders = document.querySelectorAll(".fade-in");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+}, { threshold: 0.2 });
+
+faders.forEach(el => observer.observe(el));
